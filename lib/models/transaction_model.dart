711882852015692d9ed566b856e400a911e0776a
GuillermoTorrealba <gntorrealbam@gmail.com>
2026-02-27 10:48:34 -0300

    creacion app

---
+++ lib/models/transaction_model.dart
@@ -0,0 +1,41 @@
+enum TransactionType { gasto, honorario, abono }
+
+class FinancialTransaction {
+  final String id;
+  final String description;
+  final double amount;
+  final DateTime date;
+  final TransactionType type;
+  final bool isReimbursable;
+
+  FinancialTransaction({
+    required this.id,
+    required this.description,
+    required this.amount,
+    required this.date,
+    required this.type,
+    this.isReimbursable = true,
+  });
+}
+
+class LegalCase {
+  final String id;
+  final String title;
+  final String clientName;
+  final List<FinancialTransaction> transactions;
+
+  LegalCase({
+    required this.id,
+    required this.title,
+    required this.clientName,
+    required this.transactions, // Cambiado a required para evitar errores
+  });
+
+  double get totalGastos => transactions
+      .where((t) => t.type == TransactionType.gasto)
+      .fold(0, (sum, t) => sum + t.amount);
+
+  double get totalHonorarios => transactions
+      .where((t) => t.type == TransactionType.honorario)
+      .fold(0, (sum, t) => sum + t.amount);
+}
